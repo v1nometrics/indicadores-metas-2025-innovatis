ff6025a4aa2addd0deb54c615272bcbe0d0e18fe
v1nometrics <161180150+v1nometrics@users.noreply.github.com>
2025-08-28 14:56:17 -0300

    Update header format in app.py

--- app.py
+++ app.py
@@ -1898,7 +1898,7 @@ if st.session_state["authentication_status"]:
                 st.markdown("""
                     <h3 style='text-align: center; font-size: 1.2em; line-height: 1.2; 
                     height: 2.4em; display: flex; align-items: center; justify-content: center; margin-bottom: 0;'>
-                        Sistema de Análise de Emendas Parlamentares   SAEP
+                        Sistema de Análise de Emendas Parlamentares <br>SAEP<br>
                     </h3>
                 """, unsafe_allow_html=True)
                 create_circular_progress_chart(plataformas_data['OPORTUNIDADES']["andamento"], key="oportunidades_chart")
